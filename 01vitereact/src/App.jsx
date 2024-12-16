57c9e4c3374dc4182cb472a76d4bc66305698925
AkkalDhami <dhamiakkal21@gmail.com>
2024-12-16 15:24:41 +0545

    first commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+import Chai from "./test"
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>This is a simple React application.</p>
+      <Chai /> 
+    </>
+  )
+}
+
+export default App
